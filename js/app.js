9ab05bc90a8d5003314ea1f1b75bb8b681727d39
Sky Pie <sky.petersen@gmail.com>
2026-09-02 11:11:50 +0200

    Start of House Class?

---
+++ js/app.js
@@ -0,0 +1,8 @@
+class House {
+            constructor(x,y,W,H) {
+             this.x = x;
+             this.y = y;
+             this.width = W;
+             this.height = H
+            }
+        }
